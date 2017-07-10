35ad8f4c286ad0d9f122baa301abcecc961b124d
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:27:52 -0400

    update

--- downloads/scripts/sshTunnelVnc.sh
+++ downloads/scripts/sshTunnelVnc.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:27:15 EDT 2017
-#####   Purpose: bash script to pipe vnc through 
+#####   Purpose: bash script to pipe vnc through ssh tunnel
 #####   Notes: 
 #}}}***********************************************************
  ssh -C -L 5902:localhost:5901 r 
