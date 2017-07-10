36fe306dc8a1a86f0aed58b5458727da106e7ca0
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:19:36 -0400

    update

--- downloads/scripts/myWatchNoBlinkColorized.sh
+++ downloads/scripts/myWatchNoBlinkColorized.sh
@@ -1,4 +1,11 @@
-
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:18:41 EDT 2017
+#####   Purpose: bash script to watch output of an command with color
+#####   Notes: 
+#}}}***********************************************************
 
 #set -x
 
