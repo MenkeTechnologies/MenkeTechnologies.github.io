e1cfbd288c93a10c130c2a63811a27b1e87f7a6d
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:28:18 -0400

    update

--- downloads/scripts/sshTunnelVnc2.sh
+++ downloads/scripts/sshTunnelVnc2.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:27:39 EDT 2017
-#####   Purpose: bash script to pipe vnc through ssh to R 
+#####   Purpose: bash script to pipe vnc through ssh to RPi2 
 #####   Notes: 
 #}}}***********************************************************
  ssh -C -L 5903:localhost:5901 r2
