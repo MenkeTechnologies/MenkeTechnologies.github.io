dea21aa2e55b79bf582cabc416df4528d9151686
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:21:31 -0400

    update

--- downloads/scripts/iostatShower.sh
+++ downloads/scripts/iostatShower.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 #{{{                    MARK:Header
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:21:04 EDT 2017
-#####   Purpose: bash  script to  
+#####   Purpose: bash  script to show io stats
 #####   Notes: 
 #}}}***********************************************************
 
